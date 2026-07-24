40004e4669b7b2286391bac7e2caa76a6781e261
djsaxy <269601627+djsaxy@users.noreply.github.com>
2026-07-24 08:24:32 -0500

    commented out yay install and changed browser to version from pacman

--- windowmanager.sh
+++ windowmanager.sh
@@ -8,26 +8,26 @@ doas pacman -S --needed --noconfirm base-devel
 
 # Ask if remote desktop is needed
 read -p "Remote desktop needed? <y/N>" prompt
-    if [[ $prompt =~ [yY]* ]] then
+    if [[ $prompt =~ [yY]* ]]; then
 	doas pacman -S --needed --noconfirm remmina freerdp
     fi
 
 # Install yay after changing to doas from sudo
-doas sed -i 's/PACMAN_AUTH=()/PACMAN_AUTH=(doas)/' /etc/makepkg.conf
-echo "alias yay='yay --sudo doas --sudoflags -- --save'" >> ~/.bashrc
-doas pacman -S --needed --noconfirm go
-git clone https://aur.archlinux.org/yay.git ~/.config/yay
-cd ~/.config/yay
-makepkg -si
-cd
+# doas sed -i 's/PACMAN_AUTH=()/PACMAN_AUTH=(doas)/' /etc/makepkg.conf
+# echo "alias yay='yay --sudo doas --sudoflags -- --save'" >> ~/.bashrc
+# doas pacman -S --needed --noconfirm go
+# git clone https://aur.archlinux.org/yay.git ~/.config/yay
+# cd ~/.config/yay
+# makepkg -si
+# cd
 
 # Install browser(s)
-yay -S --noconfirm helium-browser-bin
-read -p "Install Google Chrome? <y/N>" prompt
-    if [[ $prompt =~ [yY]* ]]; then
-	yay -S --noconfirm google-chrome
-	echo "\n\n# Chrome\nsuper + shift\n    google-chrome" >> ~/artixsetup/sxhkd/sxhkdrc
-    fi
+doas pacman -S --noconfirm helium-browser
+# read -p "Install Google Chrome? <y/N>" prompt
+#    if [[ $prompt =~ [yY]* ]]; then
+#	yay -S --noconfirm google-chrome
+#	echo "\n\n# Chrome\nsuper + shift\n    google-chrome" >> ~/artixsetup/sxhkd/sxhkdrc
+#    fi
 
 # Make scripts executable
 # chmod +x ~/artixsetup/dwm.sh
